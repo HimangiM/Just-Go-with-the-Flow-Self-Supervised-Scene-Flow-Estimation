4835ea29bca6b660ae28573e1e2a0c3ba14316bf
Himangi Mittal <himangimittal@gmail.com>
2020-06-08 13:58:37 -0400

    Update command_evaluate_kitti.sh

--- src/commands/command_evaluate_kitti.sh
+++ src/commands/command_evaluate_kitti.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-#
-#SBATCH --job-name=kitti_evaluate
-#SBATCH --output=/home/hmittal/github-codes/Self-Supervised-Scene-Flow-Estimation/output_eval.txt
-#SBATCH --error=/home/hmittal/github-codes/Self-Supervised-Scene-Flow-Estimation/error_eval.txt
-#
-#SBATCH --partition=GPU
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
-#SBATCH --mem=10000
-#SBATCH --gres=gpu:1
-
-
 python src/evaluate_cycle_kitti.py \
     --model model_concat_upsa \
     --data data_preprocessing/kitti_self_supervised_flow \
